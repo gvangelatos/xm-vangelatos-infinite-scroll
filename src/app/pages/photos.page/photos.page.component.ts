2ad39e4727eecdae19b67b8023c5b99595df8c1d
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-04 16:37:43 +0200

    feat(XM): WIP photos page

--- src/app/pages/photos.page/photos.page.component.ts
+++ src/app/pages/photos.page/photos.page.component.ts
@@ -1,12 +1,27 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { PhotoApiService } from '../../services/photo-api/photo-api.service';
+import { LoadingSpinnerComponent } from '../../components/loading-spinner/loading-spinner.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-  imports: [],
+  imports: [LoadingSpinnerComponent, NgOptimizedImage],
   selector: 'app-photos.page',
   styleUrl: './photos.page.component.scss',
   templateUrl: './photos.page.component.html',
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PhotosPageComponent {
+  private readonly imageService = inject(PhotoApiService);
+  protected readonly images = this.imageService.photos;
+  protected readonly isLoading = this.imageService.isLoading;
+  protected readonly error = this.imageService.error;
+
+  constructor() {
+    this.loadNextPage();
+  }
+
+  protected loadNextPage() {
+    this.imageService.loadNextPage();
+  }
 }
