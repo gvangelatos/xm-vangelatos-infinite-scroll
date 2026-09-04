6e471efaba46c4b33e740dee9eeba7b629b82289
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-04 16:46:43 +0200

    feat(XM): create image component

---
+++ src/app/components/image/image.component.ts
@@ -0,0 +1,26 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { NgOptimizedImage } from '@angular/common';
+import { PicsumPhoto } from '../../models/picsum-photo.model';
+
+@Component({
+  imports: [MatIcon, NgOptimizedImage],
+  selector: 'app-image',
+  styleUrl: './image.component.scss',
+  templateUrl: './image.component.html',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ImageComponent {
+  readonly photoClickedEmitter = output<PicsumPhoto>();
+  readonly photoModel = input.required<PicsumPhoto>();
+
+  protected imageClicked() {
+    this.photoClickedEmitter.emit(this.photoModel());
+  }
+}
