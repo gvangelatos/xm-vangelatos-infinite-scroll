dec491ec16c9462c35115f176e58c58c957b2ddf
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-04 17:06:24 +0200

    fix(XM): create InfiniteScrollTriggerDirective

---
+++ src/app/directives/infinite-scroll-trigger.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InfiniteScrollTriggerDirective } from './infinite-scroll-trigger.directive';
+
+describe('InfiniteScrollTriggerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InfiniteScrollTriggerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
