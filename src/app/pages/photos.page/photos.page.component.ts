585c6906e9a280068845007c6905d793fd4f56e5
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-04 16:52:37 +0200

    feat(XM): add image component in photos page

--- src/app/pages/photos.page/photos.page.component.ts
+++ src/app/pages/photos.page/photos.page.component.ts
@@ -2,9 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PhotoApiService } from '../../services/photo-api/photo-api.service';
 import { LoadingSpinnerComponent } from '../../components/loading-spinner/loading-spinner.component';
 import { NgOptimizedImage } from '@angular/common';
+import { ImageComponent } from '../../components/image/image.component';
+import { ITEMS_PER_PAGE } from '../../constants/shared.constants';
+import { PicsumPhoto } from '../../models/picsum-photo.model';
 
 @Component({
-  imports: [LoadingSpinnerComponent, NgOptimizedImage],
+  imports: [LoadingSpinnerComponent, NgOptimizedImage, ImageComponent],
   selector: 'app-photos.page',
   styleUrl: './photos.page.component.scss',
   templateUrl: './photos.page.component.html',
@@ -16,6 +19,7 @@ export class PhotosPageComponent {
   protected readonly images = this.imageService.photos;
   protected readonly isLoading = this.imageService.isLoading;
   protected readonly error = this.imageService.error;
+  protected readonly ITEMS_PER_PAGE = ITEMS_PER_PAGE;
 
   constructor() {
     this.loadNextPage();
@@ -24,4 +28,6 @@ export class PhotosPageComponent {
   protected loadNextPage() {
     this.imageService.loadNextPage();
   }
+
+  protected toggleFavourite(imageModel: PicsumPhoto) {}
 }
