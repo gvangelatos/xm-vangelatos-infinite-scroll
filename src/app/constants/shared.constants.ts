2044b7a0aeb82c670fd13dfab650d188179cef1d
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-04 15:45:03 +0200

    feat(XM): add picsum photo model and constants for pagination

---
+++ src/app/constants/shared.constants.ts
@@ -0,0 +1,2 @@
+export const ITEMS_PER_PAGE = 30;
+export const STARTING_PAGE = 0;
---
+++ src/app/models/picsum-photo.model.ts
@@ -0,0 +1,8 @@
+export interface PicsumPhoto {
+  id: string;
+  author: string;
+  url: string;
+  download_url: string;
+  height: number;
+  width: number;
+}
