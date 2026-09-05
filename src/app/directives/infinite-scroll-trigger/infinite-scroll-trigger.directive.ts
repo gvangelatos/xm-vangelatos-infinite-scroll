8a1c83d2cbe3d17af82965fe4e7ffcfcb27f00a0
George Vangelatos <40575152+gvangelatos@users.noreply.github.com>
2026-09-05 12:47:51 +0200

    fix(XM): fix directive not firing when already on screen, fix layout shifts from image grid

--- src/app/directives/infinite-scroll-trigger/infinite-scroll-trigger.directive.ts
+++ src/app/directives/infinite-scroll-trigger/infinite-scroll-trigger.directive.ts
@@ -1,12 +1,14 @@
 import {
   afterNextRender,
   Directive,
+  effect,
   ElementRef,
   inject,
   input,
   OnDestroy,
   output,
   OutputEmitterRef,
+  signal,
 } from '@angular/core';
 import { IMG_TILE_SIZE } from '../../constants/shared.constants';
 
@@ -22,19 +24,22 @@ export class InfiniteScrollTriggerDirective implements OnDestroy {
   readonly rootMargin = input(`${IMG_TILE_SIZE}px`);
   readonly visible: OutputEmitterRef<void> = output();
   private observer?: IntersectionObserver;
+  private readonly isIntersecting = signal(false);
   constructor() {
     afterNextRender(() => {
       this.observer = new IntersectionObserver(
         (entries) => {
-          if (entries[0]?.isIntersecting && !this.disabled()) {
-            this.visible.emit();
-          }
+          this.isIntersecting.set(entries[0]?.isIntersecting ?? false);
         },
-        {
-          rootMargin: this.rootMargin(),
-        },
+        { rootMargin: this.rootMargin() },
       );
       this.observer.observe(this.el.nativeElement);
+    });
+
+    effect(() => {
+      if (this.isIntersecting() && !this.disabled()) {
+        this.visible.emit();
+      }
     });
   }
 
